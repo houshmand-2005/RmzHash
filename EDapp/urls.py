42bf1b5bb266e68481263914696f34b92aefbf15
Houshmand <77535700+houshmand-2005@users.noreply.github.com>
2022-07-19 12:02:49 +0430

    add EnCoder class base view to url

--- EDapp/urls.py
+++ EDapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('Encoder/', views.Encoder, name='Encoder'),
+    path('Encoder/', views.EnCoder.as_view(), name='Encoder'),
     path('Decoder/', views.Decoder, name='Decoder'),
     path('Warn/', views.Warn, name='Warn')
 ]
