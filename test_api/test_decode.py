580fadb18fff48f22d80540f05983fb68ffc9255
Houshmand <houshmand2005@gmail.com>
2024-02-21 13:45:26 +0330

    refactor and run tests

--- test_api/test_decode.py
+++ test_api/test_decode.py
@@ -1,8 +1,12 @@
 import requests
-urlpath = 'http://127.0.0.1:8000/api/decode/upload/'
-test_file = open("encode_text.txt", "r")
-test_response = requests.post(urlpath, files={"file_uploaded": test_file}, data={
-                              "RANDkey": "0", "nNum": "138", "zNum": "164"})
+
+URL_PATH = "http://127.0.0.1:8000/api/decode/upload/"
+with open("encode_text.txt", "r", encoding="utf-8") as file:
+    test_response = requests.post(
+        URL_PATH,
+        files={"file_uploaded": file},
+        data={"RANDkey": "0", "nNum": "138", "zNum": "380"},
+    )
 print(test_response)
 print(test_response.text)
 input("done")
--- test_api/test_encode.py
+++ test_api/test_encode.py
@@ -1,8 +1,10 @@
 import requests
-urlpath = 'http://127.0.0.1:8000/api/encode/upload/'
-test_file = open("normal_text.txt", "r")
-test_response = requests.post(
-    urlpath, files={"file_uploaded": test_file}, data={"RANDkey": "0"})
+
+URL_PATH = "http://127.0.0.1:8000/api/encode/upload/"
+with open("normal_text.txt", "r", encoding="utf-8") as file:
+    test_response = requests.post(
+        URL_PATH, files={"file_uploaded": file}, data={"RANDkey": "0"}
+    )
 print(test_response)
 print(test_response.text)
 input("done")
